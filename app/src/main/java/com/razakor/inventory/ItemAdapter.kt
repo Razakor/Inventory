bf58bc3bd6c6b2e26a98147ab76358a3e2b9dc95
Razakor <m.shevchuk.o@gmail.com>
2019-07-16 18:57:57 +0300

    added xml layouts

---
+++ app/src/main/java/com/razakor/inventory/ItemAdapter.kt
@@ -0,0 +1,65 @@
+package com.razakor.inventory
+
+import android.content.Context
+import android.content.Intent
+import android.database.sqlite.SQLiteDatabase
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+
+class ItemAdapter(_db: SQLiteDatabase, _characters: MutableList<Character>)
+    : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+
+    private val characters: MutableList<Character> = _characters
+    private val db: SQLiteDatabase = _db
+    private lateinit var context: Context
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        context = parent.context
+        val layoutId: Int = R.layout.character_list_item
+        val inflater: LayoutInflater = LayoutInflater.from(context)
+        val view: View = inflater.inflate(layoutId, parent, false)
+        return ItemViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        holder.bind(characters[position].name, characters[position].race, characters[position].clas, characters[position].lvl)
+/*
+        holder.itemView.setOnClickListener {
+            val intent = Intent(context, InventoryActivity::class.java)
+            intent.putExtra("inventory_id", characters[position].inventory.id)
+            context.startActivity(intent)
+        }
+
+        holder.itemView.setOnLongClickListener {
+            Toast.makeText(context, "Item deleted at position $position", Toast.LENGTH_LONG).show()
+            deleteCharacterFromDatabase(db, characters[position])
+            characters.removeAt(position)
+            notifyItemRemoved(position)
+            notifyDataSetChanged()
+            return@setOnLongClickListener true
+        }
+        */
+    }
+
+    override fun getItemCount(): Int {
+        return characters.size
+    }
+
+    class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private var characterName: TextView = itemView.findViewById(R.id.character_name)
+        private var characterRace: TextView = itemView.findViewById(R.id.character_race)
+        private var characterClass: TextView = itemView.findViewById(R.id.character_class)
+        private var characterLvl: TextView = itemView.findViewById(R.id.character_lvl)
+
+        fun bind(_name: String, _race: String, _class: String, _lvl: Int) {
+            characterName.text = _name
+            characterRace.text = _race
+            characterClass.text = _class
+            characterLvl.text = _lvl.toString()
+        }
+    }
+}
